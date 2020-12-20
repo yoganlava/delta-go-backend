93f0db8129b8852eec85e998694b9a5bf3a25917
ug00031 <ug00031@surrey.ac.uk>
2020-12-20 15:48:39 +0000

    fetch create

--- internal/entity/post.go
+++ internal/entity/post.go
@@ -1,1 +1,12 @@
 package entity
+
+type Post struct {
+	ID           int    `json:"id"`
+	Title        string `json:"title"`
+	Content      string `json:"content"`
+	Mature       bool   `json:"mature"`
+	ProjectID    int    `json:"project_id"`
+	SubmitUserID int    `json:"submit_user_id"`
+	CreatedAt    string `json:"created_at"`
+	UpdatedAt    string `json:"updated_at"`
+}
