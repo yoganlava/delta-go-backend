22557f9a522da9e527306f71901adc30fb800009
ug00031 <ug00031@surrey.ac.uk>
2020-12-30 20:17:57 +0000

    tiers caching, need to refactor error handling later and find better choice

--- internal/tiers/service.go
+++ internal/tiers/service.go
@@ -2,12 +2,15 @@ package tiers
 
 import (
 	"context"
+	"fmt"
 	"main/db"
 	"main/internal/dto"
 	"main/internal/entity"
 
 	"github.com/georgysavva/scany/pgxscan"
+	"github.com/go-redis/redis/v8"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/vmihailenco/msgpack/v5"
 )
 
 type TierService struct {
@@ -34,7 +37,22 @@ func (ts TierService) CreateTier(t dto.CreateTierDTO) error {
 
 func (ts TierService) FetchProjectTiers(id int) ([]*entity.Tier, error) {
 	var t []*entity.Tier
-	err := pgxscan.Select(context.Background(), ts.pool, &t, "select title, description, cover_image_id, price, project_id, created_at, updated_at from tier where project_id=$1", id)
+	val, err := db.Cache().Get(context.Background(), fmt.Sprintf(`/tiers/project/%v`, id)).Result()
+	if err == redis.Nil {
+		err = pgxscan.Select(context.Background(), ts.pool, &t, "select title, description, cover_image_id, price, project_id, created_at, updated_at from tier where project_id=$1", id)
+		if err != nil {
+			return nil, err
+		}
+		marsh, err := msgpack.Marshal(&t)
+		if err != nil {
+			return nil, err
+		}
+		go db.Cache().Set(context.Background(), fmt.Sprintf(`/tiers/project/%v`, id), marsh, 0)
+		return t, nil
+	} else if err != nil {
+		return nil, err
+	}
+	err = msgpack.Unmarshal([]byte(val), &t)
 	if err != nil {
 		return nil, err
 	}
@@ -43,9 +61,25 @@ func (ts TierService) FetchProjectTiers(id int) ([]*entity.Tier, error) {
 
 func (ts TierService) FetchTier(id int) (entity.Tier, error) {
 	var t entity.Tier
-	err := pgxscan.Get(context.Background(), ts.pool, &t, "select title, description, cover_image_id, price, project_id, created_at, updated_at from tier where id=$1", id)
+
+	val, err := db.Cache().Get(context.Background(), fmt.Sprintf(`/tiers/%v`, id)).Result()
 	if err != nil {
 		return entity.Tier{}, err
 	}
+	err = msgpack.Unmarshal([]byte(val), &t)
+	if err != nil {
+		return entity.Tier{}, err
+	}
+	if t.ID > 0 {
+		return t, nil
+	}
+
+	err = pgxscan.Get(context.Background(), ts.pool, &t, "select title, description, cover_image_id, price, project_id, created_at, updated_at from tier where id=$1", id)
+	if err != nil {
+		return entity.Tier{}, err
+	}
+	marsh, err := msgpack.Marshal(&t)
+
+	go db.Cache().Set(context.Background(), fmt.Sprintf(`/tiers/%v`, id), marsh, 0)
 	return t, nil
 }
