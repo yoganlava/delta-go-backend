09b811e2d3a2ba52f22a153509b3b1cdf076325c
arif <arif.iwamoto@gamil.com>
2020-12-31 21:05:36 +0900

    database change

--- migrations/20191217202658_init.down.sql
+++ migrations/20191217202658_init.down.sql
@@ -1,1 +1,57 @@
 -- DROP TABLE album;
+DROP TABLE payout_transaction;
+
+DROP TABLE subscription_transaction;
+
+DROP TABLE donation_transaction;
+
+DROP TABLE user_profile;
+
+DROP TABLE post_vote;
+
+DROP TABLE post_text;
+
+DROP TABLE post_tag;
+
+DROP TABLE post_poll_answer;
+
+DROP TABLE post_poll_option;
+
+DROP TABLE post_file;
+
+DROP TABLE post_link;
+
+DROP TABLE stripe_card_payment_method;
+
+DROP TABLE stripe_bank_payment_method;
+
+DROP TABLE tier_benefit;
+
+DROP TABLE comment_vote;
+
+DROP TABLE creator_access;
+
+DROP TABLE payout_method;
+
+DROP TABLE payment_method;
+
+DROP TABLE benefit;
+
+DROP TABLE goal;
+
+DROP TABLE TRANSACTION;
+
+DROP TABLE subscription;
+
+DROP TABLE comment;
+
+DROP TABLE post;
+
+DROP TABLE access;
+
+DROP TABLE project;
+
+DROP TABLE creator;
+
+DROP TABLE users;
+
